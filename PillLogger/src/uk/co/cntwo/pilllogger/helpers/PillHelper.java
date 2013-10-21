bd50c1a8451358a502ecfe3dd37bd0d985715398
Nick <nicholas.allen88@gmail.com>
2013-10-21 17:23:50 +0100

    Removed some now unused methods

--- PillLogger/src/uk/co/cntwo/pilllogger/helpers/PillHelper.java
+++ PillLogger/src/uk/co/cntwo/pilllogger/helpers/PillHelper.java
@@ -27,35 +27,9 @@ public class PillHelper {
         return dbh.getAllPills();
 	}
 	
-	public static Map<UUID, Pill> getPillMap(Context context){
-		if(_pillMap != null)
-			return _pillMap;
-		
-		getPills(context);
-		
-		return _pillMap;
-	}
-	
 	public static void addPill(Context context, Pill pill){
 		DatabaseHelper dbh = new DatabaseHelper(context);
         dbh.insertPill(pill);
 	}
-	
-	private static void savePills(Context context, List<Pill> pills){
-		File file = getPillsFile();
-		try{
-			FileOutputStream fos = new FileOutputStream(file);
-			ObjectOutputStream os = new ObjectOutputStream(fos);
-			os.writeObject(pills);
-			os.close();
-		}
-		catch(IOException e){
-			Logger.e("PillHelper.savePills", e.getMessage(), e);
-		}
-	}
-	
-	private static File getPillsFile(){
-		File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-		return new File(path, "pills");
-	}
+
 }
