2be689587c616f72126964c0a50753b47bc31d4c
Nick <nicholas.allen88@gmail.com>
2013-10-22 15:18:48 +0100

    Created adapter for main page (consumptions)

--- PillLogger/src/uk/co/cntwo/pilllogger/activities/MainActivity.java
+++ PillLogger/src/uk/co/cntwo/pilllogger/activities/MainActivity.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.List;
 
 import uk.co.cntwo.pilllogger.R;
+import uk.co.cntwo.pilllogger.adapters.ConsumptionListAdapter;
 import uk.co.cntwo.pilllogger.models.Consumption;
 
 /**
@@ -21,18 +22,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         this.setTitle("Consumption");
 
-        Consumption consumption = new Consumption("Paracetamol", new Date());
-        Consumption consumption2 = new Consumption("Ibuprofen", new Date());
-
         List<Consumption> consumptions = new ArrayList<Consumption>();
-        consumptions.add(consumption);
-        consumptions.add(consumption2);
+        for (int i = 0; i < 15; i ++) {
+            Consumption consumption = new Consumption("Paracetamol", new Date());
+            consumptions.add(consumption);
+        }
 
         ListView list = (ListView) findViewById(R.id.main_consumption_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line);
-        list.setAdapter(adapter);
-
-        adapter.add(consumption.get_pillName());
-        adapter.add(consumption2.get_pillName());
+        list.setAdapter(new ConsumptionListAdapter(this, R.layout.pill_list_item, consumptions));
     }
 }
---
+++ PillLogger/src/uk/co/cntwo/pilllogger/adapters/ConsumptionListAdapter.java
@@ -0,0 +1,63 @@
+package uk.co.cntwo.pilllogger.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import uk.co.cntwo.pilllogger.R;
+import uk.co.cntwo.pilllogger.models.Consumption;
+
+/**
+ * Created by nick on 22/10/13.
+ */
+public class ConsumptionListAdapter extends ArrayAdapter<Consumption> {
+
+    private List<Consumption> _consumptions;
+    private Activity _activity;
+    private Typeface _openSans;
+
+    public ConsumptionListAdapter(Activity activity, int textViewResourceId, List<Consumption> consumptions) {
+        super(activity, textViewResourceId, consumptions);
+        this._consumptions = consumptions;
+        this._activity = activity;
+        _openSans = Typeface.createFromAsset(activity.getAssets(), "fonts/OpenSans-Light.ttf");
+    }
+
+    public static class ViewHolder {
+        public TextView name;
+        public TextView date;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        ViewHolder holder;
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater)_activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.consumption_list_item, null);
+            holder = new ViewHolder();
+            holder.name = (TextView) v.findViewById(R.id.consumption_list_name);
+            holder.date = (TextView) v.findViewById(R.id.consumption_list_date);
+            holder.name.setTypeface(_openSans);
+            holder.date.setTypeface(_openSans);
+            v.setTag(holder);
+        }
+        else
+            holder = (ViewHolder) v.getTag();
+
+        Consumption consumption = _consumptions.get(position);
+        if (consumption != null) {
+            holder.name.setText(consumption.get_pillName());
+            holder.date.setText(new SimpleDateFormat("H:m  dd/MM").format(consumption.get_date()));
+        }
+        return v;
+    }
+}
