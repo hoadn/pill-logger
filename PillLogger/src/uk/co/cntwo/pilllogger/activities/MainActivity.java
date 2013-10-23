bc0948c7c8a0720ab51cca1fbc136ae79545916b
alex <completenutter2@gmail.com>
2013-10-23 19:56:49 +0100

    Tidied some variable names

--- PillLogger/src/uk/co/cntwo/pilllogger/activities/MainActivity.java
+++ PillLogger/src/uk/co/cntwo/pilllogger/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package uk.co.cntwo.pilllogger.activities;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.app.ActionBar;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -9,23 +8,14 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import uk.co.cntwo.pilllogger.R;
-import uk.co.cntwo.pilllogger.adapters.ConsumptionListAdapter;
 import uk.co.cntwo.pilllogger.adapters.LeftDrawerAdapter;
-import uk.co.cntwo.pilllogger.fragments.PillDetailFragment;
-import uk.co.cntwo.pilllogger.fragments.PillListFragment;
-import uk.co.cntwo.pilllogger.helpers.DatabaseHelper;
-import uk.co.cntwo.pilllogger.helpers.Logger;
 import uk.co.cntwo.pilllogger.listeners.DrawerItemClickListener;
-import uk.co.cntwo.pilllogger.models.Consumption;
-import uk.co.cntwo.pilllogger.models.Pill;
 
 /**
  * Created by nick on 22/10/13.
@@ -34,12 +24,8 @@ public class MainActivity extends FragmentActivity {
 
     private List<String> _navigationItems = new ArrayList<String>();
     private DrawerLayout _drawerLayout;
-    private ListView _drawerList;
 
     private ActionBarDrawerToggle mDrawerToggle;
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +34,8 @@ public class MainActivity extends FragmentActivity {
 
         setUpDrawerNavigation();
 
-        mTitle = mDrawerTitle = getTitle();
+        CharSequence drawerTitle;
+        CharSequence title = drawerTitle = getTitle();
         mDrawerToggle = new ActionBarDrawerToggle(this, _drawerLayout,
                 R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
 
@@ -66,17 +53,19 @@ public class MainActivity extends FragmentActivity {
         // Set the drawer toggle as the DrawerListener
         _drawerLayout.setDrawerListener(mDrawerToggle);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar bar = getActionBar();
+        if(bar != null)
+            bar.setDisplayHomeAsUpEnabled(true);
     }
 
     private void setUpDrawerNavigation() {
         _navigationItems.add(this.getResources().getString(R.string.drawer_consumption));
         _navigationItems.add(this.getResources().getString(R.string.drawer_pills));
         _drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        _drawerList = (ListView) findViewById(R.id.left_drawer);
+        ListView drawerList = (ListView) findViewById(R.id.left_drawer);
 
-        _drawerList.setAdapter(new LeftDrawerAdapter(this, _navigationItems));
-        _drawerList.setOnItemClickListener(new DrawerItemClickListener(this, _navigationItems, _drawerLayout, _drawerList));
+        drawerList.setAdapter(new LeftDrawerAdapter(this, _navigationItems));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener(this, _navigationItems, _drawerLayout, drawerList));
     }
 
     @Override
