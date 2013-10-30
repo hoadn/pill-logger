2ec339e1ebe6917dff6231db88df6fa69c2c6d02
Alex <completenutter2@gmail.com>
2013-10-30 22:24:57 +0000

    Added basics for a settings screen. WIP.

---
+++ PillLogger/src/uk/co/cntwo/pilllogger/activities/WebViewActivity.java
@@ -0,0 +1,14 @@
+package uk.co.cntwo.pilllogger.activities;
+
+import android.app.Activity;
+import android.os.Bundle;import uk.co.cntwo.pilllogger.R;
+
+/**
+ * Created by alex on 30/10/2013.
+ */
+public class WebViewActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+    }
+}
