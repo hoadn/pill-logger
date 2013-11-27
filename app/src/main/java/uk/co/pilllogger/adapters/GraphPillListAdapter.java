b503a6e340f1cd879131981f1469a6238d70430d
Alex <completenutter2@gmail.com>
2013-11-27 20:58:23 +0000

    Tidied GraphPillListAdapter code a little

--- app/src/main/java/uk/co/pilllogger/adapters/GraphPillListAdapter.java
+++ app/src/main/java/uk/co/pilllogger/adapters/GraphPillListAdapter.java
@@ -10,54 +10,48 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import uk.co.pilllogger.R;
-import uk.co.pilllogger.helpers.LayoutHelper;
-import uk.co.pilllogger.helpers.Logger;
 import uk.co.pilllogger.models.Pill;
 import uk.co.pilllogger.state.State;
-import uk.co.pilllogger.tasks.InitTestDbTask;
-import uk.co.pilllogger.views.ColourIndicator;
 
 /**
  * Created by nick on 27/11/13.
  */
 public class GraphPillListAdapter extends ArrayAdapter<Pill> {
 
-        private List<Pill> _pills;
-        private List<Pill> _consumptionPills = new ArrayList<Pill>();
-        private Activity _activity;
-        private Typeface _openSans;
-        private int _resourceId;
-        private List<Integer> _graphPills;
+    private List<Pill> _pills;
+    private Activity _activity;
+    private Typeface _openSans;
+    private int _resourceId;
+    private List<Integer> _graphPills;
 
-        public GraphPillListAdapter(Activity activity, int textViewResourceId, List<Pill> pills) {
-            super(activity, textViewResourceId, pills);
-            _activity = activity;
-            _pills = pills;
-            _resourceId = textViewResourceId;
-            _openSans = Typeface.createFromAsset(activity.getAssets(), "fonts/OpenSans-Light.ttf");
-            _graphPills = State.getSingleton().getGraphPills();
-        }
+    public GraphPillListAdapter(Activity activity, int textViewResourceId, List<Pill> pills) {
+        super(activity, textViewResourceId, pills);
+        _activity = activity;
+        _pills = pills;
+        _resourceId = textViewResourceId;
+        _openSans = Typeface.createFromAsset(activity.getAssets(), "fonts/OpenSans-Light.ttf");
+        _graphPills = State.getSingleton().getGraphPills();
+    }
 
-        public static class ViewHolder {
-            public TextView name;
-            public TextView size;
-            public TextView units;
-            public View color;
-            public CheckBox checkbox;
-        }
+    public static class ViewHolder {
+        public TextView name;
+        public TextView size;
+        public TextView units;
+        public View color;
+        public CheckBox checkbox;
+    }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
-            ViewHolder holder;
-            if (v == null) {
-                LayoutInflater inflater = (LayoutInflater)_activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = inflater.inflate(_resourceId, null);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        ViewHolder holder = null;
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater)_activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(_resourceId, null);
+            if(v != null){
                 holder = new ViewHolder();
                 holder.name = (TextView) v.findViewById(R.id.pill_list_name);
                 holder.size = (TextView) v.findViewById(R.id.pill_list_size);
@@ -69,31 +63,27 @@ public class GraphPillListAdapter extends ArrayAdapter<Pill> {
                 holder.units.setTypeface(_openSans);
                 v.setTag(holder);
             }
+        }
+        else
+            holder = (ViewHolder) v.getTag();
+
+        Pill pill = _pills.get(position);
+        if (pill != null && holder != null) {
+            holder.name.setText(pill.getName());
+            holder.size.setText(String.valueOf(pill.getSize()));
+            holder.color.setBackgroundColor(pill.getColour());
+            if (_graphPills.contains(pill.getId()))
+                holder.checkbox.setChecked(true);
             else
-                holder=(ViewHolder) v.getTag();
-
-            Pill pill = _pills.get(position);
-            if (pill != null) {
-                holder.name.setText(pill.getName());
-                holder.size.setText(String.valueOf(pill.getSize()));
-                holder.color.setBackgroundColor(pill.getColour());
-                if (_graphPills.contains(pill.getId()))
-                    holder.checkbox.setChecked(true);
-                else
-                    holder.checkbox.setChecked(false);
-            }
-            return v;
+                holder.checkbox.setChecked(false);
         }
+        return v;
+    }
 
-        @Override
-        public int getCount() {
-            if (_pills != null)
-                return _pills.size();
-            return 0;
-        }
-
-        public void updateAdapter(List<Pill> pills) {
-            _pills = pills;
-            this.notifyDataSetChanged();
-        }
+    @Override
+    public int getCount() {
+        if (_pills != null)
+            return _pills.size();
+        return 0;
+    }
 }
