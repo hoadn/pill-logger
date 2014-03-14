538c7e4b7b42dee5b8a38b23ae3a28767b278585
Nick Allen <nicholas.allen88@gmail.com>
2014-03-14 15:36:33 +0000

    Added new activity for to display stats

---
+++ app/src/main/java/uk/co/pilllogger/activities/StatsActivity.java
@@ -0,0 +1,32 @@
+package uk.co.pilllogger.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import uk.co.pilllogger.R;
+
+public class StatsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_stats);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
