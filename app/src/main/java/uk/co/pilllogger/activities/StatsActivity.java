d70ac778d921c552d7ba111647a1642ae05a8fc7
Nick Allen <nicholas.allen88@gmail.com>
2014-03-17 17:14:46 +0000

    Stats now show correct information

--- app/src/main/java/uk/co/pilllogger/activities/StatsActivity.java
+++ app/src/main/java/uk/co/pilllogger/activities/StatsActivity.java
@@ -7,9 +7,22 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.widget.TextView;
 
-import uk.co.pilllogger.R;
+import java.util.List;
 
-public class StatsActivity extends Activity {
+import uk.co.pilllogger.R;
+import uk.co.pilllogger.models.Consumption;
+import uk.co.pilllogger.models.Pill;
+import uk.co.pilllogger.stats.Statistics;
+import uk.co.pilllogger.tasks.GetConsumptionsTask;
+import uk.co.pilllogger.tasks.GetPillsTask;
+
+public class StatsActivity extends Activity implements GetPillsTask.ITaskComplete, GetConsumptionsTask.ITaskComplete {
+
+    TextView _medicineMostTaken;
+    TextView _dayMostTaken;
+    TextView _hourMostTaken;
+    TextView _averageTimeBetween;
+    TextView _longestTimeBetween;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,13 @@ public class StatsActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_stats);
 
+        _medicineMostTaken = (TextView) findViewById(R.id.stats_most_taken_medicine);
+        _dayMostTaken = (TextView) findViewById(R.id.stats_day_most_consumptions);
+        _hourMostTaken = (TextView) findViewById(R.id.stats_hour_most_consumptions);
+        _averageTimeBetween = (TextView) findViewById(R.id.stats_average_between_consumption);
+        _longestTimeBetween = (TextView) findViewById(R.id.stats_longest_between_consumption);
+
+        new GetPillsTask(this, this).execute();
     }
 
     @Override
@@ -31,4 +51,17 @@ public class StatsActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void pillsReceived(List<Pill> pills) {
+        new GetConsumptionsTask(this, this, false).execute();
+    }
+
+    @Override
+    public void consumptionsReceived(List<Consumption> consumptions) {
+        _medicineMostTaken.setText(Statistics.getMostTakenPill(consumptions).getName());
+        _dayMostTaken.setText(Statistics.getDayWithMostConsumptions(consumptions));
+        _hourMostTaken.setText(Statistics.getHourWithMostConsumptions(consumptions) + ":00");
+        _averageTimeBetween.setText(Statistics.getAverageTimeBetweenConsumptions(consumptions, this));
+        _longestTimeBetween.setText(Statistics.getLongestTimeBetweenConsumptions(consumptions, this));
+    }
 }
