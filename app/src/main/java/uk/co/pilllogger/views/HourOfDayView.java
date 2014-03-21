20e6ba449471ec11580d4848e1c95f6afd15bda8
Nick Allen <nicholas.allen88@gmail.com>
2014-03-21 16:41:10 +0000

    Removed some commented out code

--- app/src/main/java/uk/co/pilllogger/views/HourOfDayView.java
+++ app/src/main/java/uk/co/pilllogger/views/HourOfDayView.java
@@ -63,45 +63,6 @@ public class HourOfDayView extends View
         final int desiredHSpec = MeasureSpec.makeMeasureSpec(boxSize*5, MeasureSpec.EXACTLY);
         final int desiredWSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
         setMeasuredDimension(desiredWSpec, desiredHSpec);
-
-        //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-//        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-//        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-//
-//        _desiredHeight = _boxSize * 5;
-//        _desiredWidth = _width;
-//
-//        int width;
-//        int height;
-//
-//        //Measure Width
-//        if (widthMode == MeasureSpec.EXACTLY) {
-//            //Must be this size
-//            width = widthSize;
-//        } else if (widthMode == MeasureSpec.AT_MOST) {
-//            //Can't be bigger than...
-//            width = Math.min(_desiredWidth, widthSize);
-//        } else {
-//            //Be whatever you want
-//            width = _desiredWidth;
-//        }
-//
-//        //Measure Height
-//        if (heightMode == MeasureSpec.EXACTLY) {
-//            //Must be this size
-//            height = heightSize;
-//        } else if (heightMode == MeasureSpec.AT_MOST) {
-//            //Can't be bigger than...
-//            height = Math.min(_desiredHeight, heightSize);
-//        } else {
-//            //Be whatever you want
-//            height = _desiredHeight;
-//        }
-//
-//        //MUST CALL THIS
-//        setMeasuredDimension(width, height);
     }
 
     private void preInit(Context context) {
