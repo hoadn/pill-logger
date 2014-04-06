cb714bd2d00db394ecea308843098b0f1d76fdf0
Nicholas Allen <nicholas.allen88@gmail.com>
2014-04-06 12:11:17 +0100

    Added a set as favourite option in the pill dialog

--- app/src/main/java/uk/co/pilllogger/dialogs/PillInfoDialog.java
+++ app/src/main/java/uk/co/pilllogger/dialogs/PillInfoDialog.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import uk.co.pilllogger.R;
 import uk.co.pilllogger.models.Pill;
 import uk.co.pilllogger.state.State;
+import uk.co.pilllogger.tasks.UpdatePillTask;
 
 /**
  * Created by Alex on 05/03/14.
@@ -33,10 +34,14 @@ public class PillInfoDialog extends InfoDialog {
 
         TextView addConsumption = (TextView) view.findViewById(R.id.info_dialog_add_consumption);
         TextView delete = (TextView) view.findViewById(R.id.info_dialog_delete);
+        TextView favourite = (TextView) view.findViewById(R.id.info_dialog_set_favourite);
+        if (_pill.isFavourite())
+            favourite.setText(getResources().getString(R.string.info_dialog_unset_favourite));
 
         Typeface typeface = State.getSingleton().getTypeface();
         addConsumption.setTypeface(typeface);
         delete.setTypeface(typeface);
+        favourite.setTypeface(typeface);
 
         if(_pill == null) {
             dismiss();
@@ -55,10 +60,18 @@ public class PillInfoDialog extends InfoDialog {
                 _listener.onDialogDelete(_pill, PillInfoDialog.this);
             }
         });
+        favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _listener.setDialogFavourite(_pill, PillInfoDialog.this);
+            }
+
+        });
     }
 
     public interface PillInfoDialogListener {
         public void onDialogAddConsumption(Pill pill, InfoDialog dialog);
         public void onDialogDelete(Pill pill, InfoDialog dialog);
+        public void setDialogFavourite(Pill pill, InfoDialog dialog);
     }
 }
