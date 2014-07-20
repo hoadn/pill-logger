1f3b4c0dcc4ef6bfe5984019c3e6d8683bafcb99
Alex <completenutter2@gmail.com>
2014-07-20 20:11:47 +0100

    Updated crashlytics logging

--- app/src/main/java/uk/co/pilllogger/helpers/Logger.java
+++ app/src/main/java/uk/co/pilllogger/helpers/Logger.java
@@ -28,43 +28,59 @@ public class Logger {
 	}
 
     public static void i(String tag, String message){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.i(tag, message);
+            Crashlytics.log(message);
+        }
     }
 
     public static void i(String tag, String message, Throwable ex){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.i(tag, message, ex);
+            Crashlytics.log(message);
+        }
     }
 
     public static void d(String tag, String message){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose){
             android.util.Log.d(tag, message);
+            Crashlytics.log(message);
+        }
     }
 
     public static void d(String tag, String message, Throwable ex){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.d(tag, message, ex);
+            Crashlytics.log(message);
+        }
     }
 
     public static void w(String tag, String message){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.w(tag, message);
+            Crashlytics.log(message);
+        }
     }
 
     public static void w(String tag, String message, Throwable ex){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.w(tag, message, ex);
+            Crashlytics.log(message);
+        }
     }
 
     public static void v(String tag, String message){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.v(tag, message);
+            Crashlytics.log(message);
+        }
     }
 
     public static void v(String tag, String message, Throwable ex){
-        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose)
+        if(_logLevel == LogLevel.Information || _logLevel == LogLevel.Verbose) {
             android.util.Log.v(tag, message, ex);
+            Crashlytics.log(message);
+        }
     }
 
     private enum LogLevel{
