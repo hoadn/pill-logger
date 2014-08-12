68a12341f091c6dcdbfaf3e7dccf08ae0f7685a1
Alex <completenutter2@gmail.com>
2014-08-12 22:37:16 +0100

    Started work on new L styling

--- app/src/main/java/uk/co/pilllogger/adapters/ConsumptionListAdapter.java
+++ app/src/main/java/uk/co/pilllogger/adapters/ConsumptionListAdapter.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -238,7 +239,7 @@ public class ConsumptionListAdapter extends ActionBarArrayAdapter<Consumption> {
                     holder.quantity.setText(String.valueOf(consumption.getQuantity()));
                     holder.consumption = consumption;
 
-                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.container.getLayoutParams();
+                    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) holder.container.getLayoutParams();
                     DateTime currentDate = new DateTime(consumption.getDate());
 
                     holder.dayText.setText("");
